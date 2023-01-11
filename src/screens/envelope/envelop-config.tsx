d09c7976c70aa84fc7d3aa26417f9accf32dbe0c
Sebajuste <sebajuste@hotmail.com>
2023-01-11 19:28:22 +0100

    Fix issue for some properties for envelope creation

--- src/screens/envelope/envelop-config.tsx
+++ src/screens/envelope/envelop-config.tsx
@@ -7,7 +7,6 @@ import { EnvelopeDaoStorage } from "../../services/async_storage/budget_async_st
 import { budgetPerMonth, Envelope, EnvelopeCategory, Period, periodFromString, periodToString } from "../../services/budget";
 import uuid from 'react-native-uuid';
 import _ from "lodash";
-import { resolveUri } from "expo-asset/build/AssetSources";
 
 
 const operation_type_picker_items = [
@@ -19,104 +18,11 @@ const operation_type_picker_items = [
 
 
 
-
-export function EnvelopForm({envelope, onChange, children} : {envelope: Envelope, onChange: (envelope: Envelope) => void, children?: any}) {
-
-    const [amount, setAmount] = useState( `${envelope.amount}`);
-
-    const [env, setEnv] = useState(envelope);
-
-
-    const nameHandler = (name: string) => {
-        env.name = name;
-        setEnv({...env, name: name});
-        onChange(env);
-    };
-
-    const amountHandler = (amount: string) => {
-        env.amount = parseFloat(amount);
-        setAmount(amount);
-        setEnv({...env, amount: env.amount});
-        onChange(env);
-    };
-
-    const periodHandler = (period: Period) => {
-        env.period = period;
-        setEnv({...env, period: env.period});
-        onChange(envelope);
-    };
-
-    const dueDateHandler = (date: Date) => {
-        env.dueDate = date;
-        setEnv({...env, dueDate: env.dueDate});
-        onChange(envelope);
-    };
-
-    // const showDueDate = env.period != Period.MONTH;
-
-    return (
-        <Layout style={{margin: 10}}>
-        
-            <View style={{ flex: 1 }}>
-
-                <View style={{ flexDirection: 'row' }}>
-
-                    <View style={{flex: 1, margin: 2}}>
-                        <Text style={{ fontSize: 12 }}>Envelope name</Text>
-                        <TextInput
-                            placeholder="Enter the envelope name"
-                            value={env.name}
-                            onChangeText={nameHandler}
-                        />
-                    </View>
-
-                    <View style={{flex: 1, margin: 2}}>
-                        <Text style={{ fontSize: 12 }}>Budget Amount</Text>
-                        <TextInput
-                            placeholder="0.00"
-                            value={amount}
-                            onChangeText={amountHandler}
-                        />
-                    </View>
-
-                </View>
-
-                <View style={{ flexDirection: 'row' }}>
-
-                    <View style={{flex: 1, margin: 2}}>
-                        <Text style={{ fontSize: 12 }}>Budget Period</Text>
-                        <Picker placeholder="Period" items={operation_type_picker_items} value={ periodToString(env.period) } onValueChange={(val: string) =>  periodHandler( periodFromString(val) )} ></Picker>
-                    </View>
-
-                    <View style={{flex: 1, margin: 10}}>
-                        <Text style={{ flex: 1, alignItems: "flex-end", color: '#888'}}>{ budgetPerMonth(env.amount, env.period).toFixed(2) } </Text>
-                        <Text style={{ flex: 2, fontSize: 12, color: '#888' }}>Montly</Text>
-                    </View>
-
-                </View>
-                <View>
-
-                    <View>
-                        <SelectDateComponent label="Due Date" date={env.dueDate ? (typeof env.dueDate === 'string' ? new Date(env.dueDate) : env.dueDate) : new Date()} onChange={dueDateHandler} />
-                    </View>
-
-                </View>
-
-
-            </View>
-
-            {children}
-            
-        </Layout>
-    );
-
-}
-
 export function EnvelopeConfigScreen({ navigation, route } : {navigation : any, route : any}) {
 
     const envelopeCategory : EnvelopeCategory = route.params?.envelopeCategory;
 
-    const [envelope, setEnvelope] = useState<Envelope>( route.params?.envelope || {
+    const envelope : Envelope = route.params?.envelope || {
         _id: uuid.v4(),
         name: '',
         amount: 0,
@@ -124,65 +30,9 @@ export function EnvelopeConfigScreen({ navigation, route } : {navigation : any, 
         period: Period.MONTH,
         dueDate: new Date(),
         category_id: envelopeCategory._id
-    } as Envelope );
+    } as Envelope;
 
     const updateForm = route.params.envelope ? true : false;
-
-    const envelopeDao = new EnvelopeDaoStorage();
-
-    const changeHandler = (envelope: Envelope) => {
-        setEnvelope(envelope);
-    };
-
-    const addHandler = () => {
-        envelopeDao.add(envelope).then(v => {
-            const popAction = StackActions.pop(1);
-            navigation.dispatch(popAction);
-        });
-    }
-
-    const updateHandler = () => {
-        envelopeDao.load().then(envelopes => {
-            const env = _.find(envelopes, item => item._id == envelope._id);
-            if( env ) {
-                env.name = envelope.name;
-                env.amount = envelope.amount;
-                env.period = envelope.period;
-                env.dueDate = envelope.dueDate;
-                return envelopeDao.save(envelopes).then(v => {
-                    const popAction = StackActions.pop(1);
-                    navigation.dispatch(popAction);
-                });
-            }
-        });
-    }
-
-    const deleteHandler = () => {
-        envelopeDao.remove(envelope).then(v => {
-            const popAction = StackActions.pop(1);
-            navigation.dispatch(popAction);
-        });
-    };
-
-    const formValid = envelope.name.trim().length > 0 && envelope.amount > 0 ;
-
-    // { updateForm ? <Button style={{margin: 5, flexGrow: 1}} text="DELETE" status="danger" onPress={deleteHandler}></Button> : <></> }
-    return (
-        <EnvelopForm envelope={envelope} onChange={changeHandler}>
-            <View style={{ flexDirection: 'row'}} >
-                <Button style={{margin: 5, flexGrow: 1}} text="SAVE" status="primary" disabled={!formValid} onPress={updateForm ? updateHandler : addHandler}></Button>
-            </View>
-        </EnvelopForm>
-    );
-
-}
-
-/*
-export function EnvelopeCreateScreen({ navigation, route } : {navigation : any, route : any}) {
-
-    const envelopeCategory : EnvelopeCategory = route.params?.envelopeCategory;
-
-    const envelope : Envelope = route.params?.envelope;
 
     const [name, setName] = useState( envelope ? envelope.name : '');
 
@@ -190,15 +40,13 @@ export function EnvelopeCreateScreen({ navigation, route } : {navigation : any, 
 
     const [period, setPeriod] = useState(  envelope ? envelope.period : Period.MONTH );
 
-    const [dueDate, setDueDate] = useState( envelope && envelope.dueDate ? envelope.dueDate : new Date() );
+    const [dueDate, setDueDate] = useState( envelope && envelope.dueDate ? (typeof envelope.dueDate === 'string' ? new Date(envelope.dueDate) : envelope.dueDate) : new Date() );
 
     const showDueDate = period != Period.MONTH;
 
-    const formValid = name.trim().length > 0 && amount.trim().length > 0 && parseFloat(amount) != 0;
+    const envelopeDao = new EnvelopeDaoStorage();
 
-    const saveHandler = () => {
-
-        const envelopeDao = new EnvelopeDaoStorage();
+    const addHandler = () => {
 
         const envelope : Envelope = {
             _id: uuid.v4(),
@@ -217,9 +65,30 @@ export function EnvelopeCreateScreen({ navigation, route } : {navigation : any, 
 
     };
 
-    const deleteHandler = () => {
+    const updateHandler = () => {
+        envelopeDao.load().then(envelopes => {
+            const env = _.find(envelopes, item => item._id == envelope._id);
+            if( env ) {
+                env.name = name;
+                env.amount = parseFloat(amount);
+                env.period = period;
+                env.dueDate = dueDate;
+                return envelopeDao.save(envelopes).then(v => {
+                    const popAction = StackActions.pop(1);
+                    navigation.dispatch(popAction);
+                });
+            }
+        });
+    }
 
+    const deleteHandler = () => {
+        envelopeDao.remove(envelope).then(v => {
+            const popAction = StackActions.pop(1);
+            navigation.dispatch(popAction);
+        });
     };
+
+    const formValid = name.trim().length > 0 && amount.trim().length > 0 && parseFloat(amount) != 0;
 
     return (
         <Layout style={{margin: 10}}>
@@ -273,12 +142,11 @@ export function EnvelopeCreateScreen({ navigation, route } : {navigation : any, 
             </View>
 
             <View style={{ flexDirection: 'row'}} >
-                { envelope ? <Button style={{margin: 5, flexGrow: 1}} text="DELETE" status="danger" onPress={() => deleteHandler()}></Button> : <></> }
-                <Button style={{margin: 5, flexGrow: 1}} text="SAVE" status="primary" disabled={!formValid} onPress={() => saveHandler()}></Button>
+            { updateForm ? <Button style={{margin: 5, flexGrow: 1}} text="DELETE" status="danger" onPress={deleteHandler}></Button> : <></> }
+                <Button style={{margin: 5, flexGrow: 1}} text="SAVE" status="primary" disabled={!formValid} onPress={updateForm ? updateHandler : addHandler}></Button>
             </View>
             
         </Layout>
     );
 
 }
-*/
